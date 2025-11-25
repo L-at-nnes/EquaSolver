322630b342348870f5c31b7277d4754f708b4935
l-at-nnes <201150075+l-at-nnes@users.noreply.github.com>
2025-11-25 01:34:47 +0100

    Fix input bug: change all number inputs to text with inputmode=decimal to allow complete value clearing and free typing

--- js/script.js
+++ js/script.js
@@ -1272,11 +1272,11 @@ function updateGraphInputs() {
             html = `
                 <div class="graph-input-group">
                     <label>a:</label>
-                    <input type="number" id="graphA" value="1" step="0.1" class="graph-input">
+                    <input type="text" inputmode="decimal" id="graphA" value="1" step="0.1" class="graph-input">
                 </div>
                 <div class="graph-input-group">
                     <label>b:</label>
-                    <input type="number" id="graphB" value="0" step="0.1" class="graph-input">
+                    <input type="text" inputmode="decimal" id="graphB" value="0" step="0.1" class="graph-input">
                 </div>
             `;
             break;
@@ -1284,15 +1284,15 @@ function updateGraphInputs() {
             html = `
                 <div class="graph-input-group">
                     <label>a:</label>
-                    <input type="number" id="graphA" value="1" step="0.1" class="graph-input">
+                    <input type="text" inputmode="decimal" id="graphA" value="1" step="0.1" class="graph-input">
                 </div>
                 <div class="graph-input-group">
                     <label>b:</label>
-                    <input type="number" id="graphB" value="0" step="0.1" class="graph-input">
+                    <input type="text" inputmode="decimal" id="graphB" value="0" step="0.1" class="graph-input">
                 </div>
                 <div class="graph-input-group">
                     <label>c:</label>
-                    <input type="number" id="graphC" value="0" step="0.1" class="graph-input">
+                    <input type="text" inputmode="decimal" id="graphC" value="0" step="0.1" class="graph-input">
                 </div>
             `;
             break;
@@ -1300,19 +1300,19 @@ function updateGraphInputs() {
             html = `
                 <div class="graph-input-group">
                     <label>a:</label>
-                    <input type="number" id="graphA" value="1" step="0.1" class="graph-input">
+                    <input type="text" inputmode="decimal" id="graphA" value="1" step="0.1" class="graph-input">
                 </div>
                 <div class="graph-input-group">
                     <label>b:</label>
-                    <input type="number" id="graphB" value="0" step="0.1" class="graph-input">
+                    <input type="text" inputmode="decimal" id="graphB" value="0" step="0.1" class="graph-input">
                 </div>
                 <div class="graph-input-group">
                     <label>c:</label>
-                    <input type="number" id="graphC" value="0" step="0.1" class="graph-input">
+                    <input type="text" inputmode="decimal" id="graphC" value="0" step="0.1" class="graph-input">
                 </div>
                 <div class="graph-input-group">
                     <label>d:</label>
-                    <input type="number" id="graphD" value="0" step="0.1" class="graph-input">
+                    <input type="text" inputmode="decimal" id="graphD" value="0" step="0.1" class="graph-input">
                 </div>
             `;
             break;
